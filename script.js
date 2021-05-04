d788bce657f8313b4a47c4291efeac5be7191ec6
rafaeldellaquila <rafadellaquila@gmail.com>
2021-05-04 17:37:20 -0300

    :zap: linking images to descriptions

---
+++ script.js
@@ -0,0 +1,26 @@
+const AnimalImageContent = document.querySelectorAll(
+  '.animalSection > .images li'
+); //chamando minhas tags de imagens
+const AnimalDescriptionContent = document.querySelectorAll(
+  '.animalSection > .descriptions > .description'
+); //chamando minhas tags de texto
+
+//checando se tem, pra não bugar o site caso não entre as classes
+if (AnimalImageContent.length && AnimalDescriptionContent.length) {
+  //pra deixar ativo o primeiro, sim, eu sei que da pra fazer isso direto no css também
+  AnimalDescriptionContent[0].classList.add('active');
+
+  //ativando e desativando a classe que vai mostrar ou não conteudos de texto
+  function activeContent(index) {
+    AnimalDescriptionContent.forEach((description) => {
+      description.classList.remove('active');
+    });
+    AnimalDescriptionContent[index].classList.add('active');
+  }
+  //loop que irá linkar através do index a imagem ao texto
+  AnimalImageContent.forEach((image, index) => {
+    image.addEventListener('click', () => {
+      activeContent(index);
+    });
+  });
+}
