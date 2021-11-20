5f58928533dd514fb5bdc58314959bed3e3346dd
Rafael Dell'Aquila <rafael@dellaquila.dev>
2021-11-20 16:32:29 -0300

    :sparkles: modal functions  ready

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,10 @@ import linkToImage from './modules/linkToImage'
 import smoothScroll from './modules/smoothScroll'
 import accordionList from './modules/accordionList'
 import scrollAnimation from './modules/scrollAnimation'
+import initModal from './modules/modal'
 
 linkToImage()
 accordionList()
 smoothScroll()
 scrollAnimation()
+initModal()
---
+++ src/js/modules/modal.js
@@ -0,0 +1,20 @@
+export default function initModal() {
+  const openBtn = document.querySelector('[data-modal=open]')
+  const closeBtn = document.querySelector('[data-modal=close]')
+  const containerModal = document.querySelector('[data-modal=container')
+
+  if (openBtn && closeBtn && containerModal) {
+    function toggleModal(event) {
+      event.preventDefault()
+      containerModal.classList.toggle('active')
+    }
+
+    function clickOutModal(event) {
+      if (event.target === this) toggleModal(event)
+    }
+
+    openBtn.addEventListener('click', toggleModal)
+    closeBtn.addEventListener('click', toggleModal)
+    containerModal.addEventListener('click', clickOutModal)
+  }
+}
